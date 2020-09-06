7633c53924d99dff60f93e6357021ac8479fdddd
z217 <zihan.zhouchn@outlook.com>
2020-09-06 21:35:45 +0800

    add two blogs and modify css of toc, 2020-09-06

--- js/tocbutton.js
+++ js/tocbutton.js
@@ -3,15 +3,21 @@ function tocToggle() {
 }
 
 function isTocOpen() {
-    tocStyle = window.getComputedStyle(document.getElementById("sidetoc"));
-    tocBottom = tocStyle.getPropertyValue('bottom');
-    return tocBottom === "150px";
+    return document.getElementById("sidetoc").style.visibility !== "hidden";
 }
 
 function tocOpen() {
-    document.getElementById("sidetoc").style.bottom = "150px";
+    document.getElementById("tocbtn").style.right = "100px";
+    let sideStyle = document.getElementById("sidetoc").style;
+    sideStyle.visibility = "visible";
+    sideStyle.opacity = 1;
+    sideStyle.maxHeight = "1000px";
 }
 
 function tocClose() {
-    document.getElementById("sidetoc").style.bottom = (window.innerHeight - 130) + 'px';
+    let sideStyle = document.getElementById("sidetoc").style;
+    sideStyle.visibility = "hidden";
+    sideStyle.opacity = 0;
+    sideStyle.maxHeight = "0px";
+    document.getElementById("tocbtn").style.right = "0px";
 }
