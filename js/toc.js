99e2c0a55dd8c77bfedf55678fa61e2c16381703
z217 <zihan.zhouchn@outlook.com>
2021-02-19 12:38:12 +0800

    fix toc bug, 2020-02-19

--- js/toc.js
+++ js/toc.js
@@ -1,6 +1,7 @@
 let toc;
+let tocFlag = true;
 
-new function () {
+function tocInit() {
     let ul = $('#TableOfContents > ul');
     let headers = $(':header');
     let curLevel = 2;
@@ -32,10 +33,55 @@ new function () {
         'scrollTimer': undefined,
         'setScrollTimer': function () {
             setTimeout(() => {
-                lightAnchor($(window).scrollTop());
+                toc.lightAnchor($(window).scrollTop());
             }, 300);
+        },
+        'lightAnchor': function (scrollTop) {
+            if (scrollTop >= toc.offsetHs[toc.offsetHs.length - 1]) {
+                if (!toc.clickFlag)
+                    toc.cur = toc.aArr.length - 1;
+            } else {
+                for (let i = 1; i < toc.offsetHs.length; i++) {
+                    if (scrollTop >= toc.offsetHs[i - 1] && scrollTop < toc.offsetHs[i]) {
+                        if (!toc.clickFlag) toc.cur = i - 2 <= 0 ? 0 : i - 2;
+                        break;
+                    }
+                }
+            }
+            $(toc.aArr).css('color', 'rgba(238, 238, 238, 0.664)');
+            $(toc.aArr[toc.cur]).css('color', 'white');
+            let top = $(toc.aArr[toc.cur]).position().top;
+            if (top > toc.sideToc.height() || top < 0) {
+                toc.sideToc.animate({
+                        scrollTop: top + "px"
+                    }, 400,
+                    "linear");
+            }
+        },
+        'tocToggle': function () {
+            toc.isTocOpen() ? toc.tocClose() : toc.tocOpen();
+        },
+        'isTocOpen': function () {
+            return toc.sideToc.css('visibility') !== "hidden";
+        },
+        'tocOpen': function () {
+            toc.tocBtn.css('right', '100px');
+            toc.sideToc.css({
+                'visibility': 'visible',
+                'opacity': '1',
+                'maxHeight': '1000px'
+            });
+        },
+        'tocClose': function () {
+            toc.tocBtn.css('right', '0px');
+            toc.sideToc.css({
+                'visibility': 'hidden',
+                'opacity': '0',
+                'maxHeight': '0px'
+            });
         }
     };
+
     let sideToc = toc.sideToc;
     let aArr = toc.aArr;
     // 调整导航栏高度
@@ -45,90 +91,39 @@ new function () {
             sideToc.css('height', i + 30 + 'px');
         }
     }
-    // 悬浮高亮
-    for (let i = 0; i < aArr.length; i++) {
-        let hid = $(aArr[i]).attr('href');
-        toc.offsetHs.push($(hid).offset().top);
-        $(aArr[i]).hover(function () {
-            $(this).css('color', 'white');
-        }, function () {
-            $(this).css('color', 'rgba(238, 238, 238, 0.664)');
-            $(aArr[toc.cur]).css('color', 'white');
-        });
-    }
+    // 记录标题高度
+    for (let i = 0; i < aArr.length; i++)
+        toc.offsetHs.push($($(aArr[i]).attr('href')).offset().top - 10);
+    // 设置定时器
     toc.scrollTimer = toc.setScrollTimer();
     // 小窗口隐藏
     if (window.innerWidth <= 800) tocClose();
-}();
+    // 初始化事件
+    $('#tocbtn').click(toc.tocToggle);
+    toc.aArr.hover(aArrHoverIn, aArrHoverOut);
+    toc.aArr.click(aArrClick);
+    $(window).on('scroll', () => {
+        clearTimeout(toc.scrollTimer);
+        toc.setScrollTimer();
+    });
+};
 
-toc.aArr.click(function () {
+function aArrHoverIn() {
+    $(this).css('color', 'white');
+}
+
+function aArrHoverOut() {
+    $(this).css('color', 'rgba(238, 238, 238, 0.664)');
+    $(toc.aArr[toc.cur]).css('color', 'white');
+}
+
+function aArrClick() {
     toc.clickFlag = true;
     $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top + "px"
+            'scrollTop': $($(this).attr('href')).offset().top + 'px'
         }, 400,
         "linear",
-        function () {
-            toc.clickFlag = false;
-        });
+        () => toc.clickFlag = false
+    );
     return false;
-});
-
-$(window).on('scroll', () => {
-    clearTimeout(toc.scrollTimer);
-    toc.setScrollTimer();
-});
-
-function lightAnchor(scrollTop) {
-    let aArr = toc.aArr;
-    let offsetHs = toc.offsetHs;
-    if (scrollTop >= offsetHs[offsetHs.length - 1]) {
-        if (!toc.clickFlag)
-            toc.cur = aArr.length - 1;
-    } else {
-        for (let i = 1; i < offsetHs.length; i++) {
-            if (scrollTop >= offsetHs[i - 1] && scrollTop < offsetHs[i]) {
-                if (!toc.clickFlag) {
-                    toc.cur = i - 2 <= 0 ? 0 : i - 2;
-                }
-                break;
-            }
-        }
-    }
-    $(aArr).css('color', 'rgba(238, 238, 238, 0.664)');
-    $(aArr[toc.cur]).css('color', 'white');
-    let top = $(aArr[toc.cur]).position().top;
-    if (top > toc.sideToc.height() || top < 0) {
-        toc.sideToc.animate({
-                scrollTop: top + "px"
-            }, 400,
-            "linear");
-    }
-}
-
-
-
-function tocToggle() {
-    isTocOpen() ? tocClose() : tocOpen();
-}
-
-function isTocOpen() {
-    return toc.sideToc.css('visibility') !== "hidden";
-}
-
-function tocOpen() {
-    toc.tocBtn.css('right', '100px');
-    toc.sideToc.css({
-        'visibility': 'visible',
-        'opacity': '1',
-        'maxHeight': '1000px'
-    });
-}
-
-function tocClose() {
-    toc.tocBtn.css('right', '0px');
-    toc.sideToc.css({
-        'visibility': 'hidden',
-        'opacity': '0',
-        'maxHeight': '0px'
-    });
 }
