e34a9b154f11db6c3c75e0bb156b6ee6bc61b41d
z217 <zihan.zhouchn@outlook.com>
2021-10-29 15:29:51 +0800

    fix unmerge

--- js/toc.js
+++ js/toc.js
@@ -4,10 +4,7 @@ let tocFlag = true;
 function tocInit() {
     let ul = $('#TableOfContents > ul');
     let headers = $(':header');
-<<<<<<< HEAD
-=======
     let tocLiMap = new Map();
->>>>>>> modify js
     let curLevel = 2;
     for (let i = 1; i < headers.length - 1; i++) {
         let level = $(headers[i]).prop('tagName')[1];
@@ -24,51 +21,32 @@ function tocInit() {
                 curLevel--;
             } while (curLevel > level);
         }
-<<<<<<< HEAD
-        ul.append('<li><a href="#' + $(headers[i]).attr('id') + '">' + $(headers[i]).text() + '</a></li>');
-=======
         let li = $('<li id="toc-li"><a href="#' + $(headers[i]).attr('id') + '">' + $(headers[i]).text() + '</a></li>');
         // ul.append('<li id="toc-li"><a href="#' + $(headers[i]).attr('id') + '">' + $(headers[i]).text() + '</a></li>');
         ul.append(li)
         tocLiMap.set($(headers[i]).text(), li)
->>>>>>> modify js
     }
 
     toc = {
         'clickFlag': false,
-<<<<<<< HEAD
-        'offsetHs': [0],
-        'aArr': $('#sidetoc a'),
-        'sideToc': $('#sidetoc'),
-=======
         'aArr': $('#sidetoc a'),
         'offsetHs': undefined,
         'sideToc': $('#sidetoc'),
         'tocLiMap': tocLiMap,
->>>>>>> modify js
         'tocBtn': $('#tocbtn'),
         'cur': 0,
         'scrollTimer': undefined,
         'setScrollTimer': function () {
-<<<<<<< HEAD
-            setTimeout(() => {
-                toc.lightAnchor($(window).scrollTop());
-            }, 300);
-=======
             return setTimeout(() => {
                 toc.lightAnchor($(window).scrollTop());
             }, 500);
->>>>>>> modify js
         },
         'lightAnchor': function (scrollTop) {
             if (scrollTop >= toc.offsetHs[toc.offsetHs.length - 1]) {
                 if (!toc.clickFlag)
                     toc.cur = toc.aArr.length - 1;
-<<<<<<< HEAD
-=======
             } else if (scrollTop <= 0) {
                 toc.cur = 0;
->>>>>>> modify js
             } else {
                 for (let i = 1; i < toc.offsetHs.length; i++) {
                     if (scrollTop >= toc.offsetHs[i - 1] && scrollTop < toc.offsetHs[i]) {
@@ -79,20 +57,11 @@ function tocInit() {
             }
             $(toc.aArr).css('color', 'rgba(238, 238, 238, 0.664)');
             $(toc.aArr[toc.cur]).css('color', 'white');
-<<<<<<< HEAD
-            let top = $(toc.aArr[toc.cur]).position().top;
-            if (top > toc.sideToc.height() || top < 0) {
-                toc.sideToc.animate({
-                        scrollTop: top + "px"
-                    }, 400,
-                    "linear");
-=======
             let top = toc.tocLiMap.get(toc.aArr[toc.cur].text).position().top;
             if (top > toc.sideToc.height() || top < 0) {
                 toc.sideToc.animate({
                     scrollTop: "+=" + top + "px"
                 }, 400, "linear");
->>>>>>> modify js
             }
         },
         'tocToggle': function () {
@@ -116,8 +85,6 @@ function tocInit() {
                 'opacity': '0',
                 'maxHeight': '0px'
             });
-<<<<<<< HEAD
-=======
         },
         'getHeaderHeights': function(offsetHs, aArr) {
             for (let i = 0; i < aArr.length; i++)
@@ -127,16 +94,12 @@ function tocInit() {
             return setTimeout(() => {
                 toc.getHeaderHeights(toc.offsetHs, toc.aArr);
             }, 500);
->>>>>>> modify js
         }
     };
 
     let sideToc = toc.sideToc;
     let aArr = toc.aArr;
-<<<<<<< HEAD
-=======
     toc.offsetHs = new Array(aArr.length + 1)
->>>>>>> modify js
     // 调整导航栏高度
     if (sideToc.length > 0) {
         let i = sideToc.find("header").innerHeight() + sideToc.find("nav").innerHeight();
@@ -145,13 +108,8 @@ function tocInit() {
         }
     }
     // 记录标题高度
-<<<<<<< HEAD
-    for (let i = 0; i < aArr.length; i++)
-        toc.offsetHs.push($($(aArr[i]).attr('href')).offset().top - 10);
-=======
     toc.getHeaderHeights(toc.offsetHs, toc.aArr)
     toc.setHeaderHeightsTimer()
->>>>>>> modify js
     // 设置定时器
     toc.scrollTimer = toc.setScrollTimer();
     // 小窗口隐藏
@@ -162,11 +120,7 @@ function tocInit() {
     toc.aArr.click(aArrClick);
     $(window).on('scroll', () => {
         clearTimeout(toc.scrollTimer);
-<<<<<<< HEAD
-        toc.setScrollTimer();
-=======
         toc.scrollTimer = toc.setScrollTimer();
->>>>>>> modify js
     });
     $(window).on('resize', () => {
         for (let i = 0; i < aArr.length; i++)
